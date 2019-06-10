e9ef3151d9e02b940cce418bb6c125fca6c63868
Schaco <zhangranmm@126.com>
2019-06-10 18:29:37 +0800

    增加添加水印功能

--- SchacoPDFViewer/SchacoPDFViewer/Model/IExcelToPDF/AsposeE2P.cs
+++ SchacoPDFViewer/SchacoPDFViewer/Model/IExcelToPDF/AsposeE2P.cs
@@ -1,4 +1,5 @@
 ﻿using Aspose.Cells;
+using Aspose.Cells.Drawing;
 using Aspose.Cells.Rendering;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,7 @@ namespace SchacoPDFViewer
 
         public void TurnToPDF(string sourceExcelFile, string targetPdfFile)
         {
+            AddWaterMark(sourceExcelFile);
             try
             {
                 if (!File.Exists(sourceExcelFile))
@@ -36,6 +38,64 @@ namespace SchacoPDFViewer
             {
                 MyLogger.LoggerInstance.Error(ex);
             }
+        }
+
+        void AddWaterMark(string sourceExcelFile)
+        {
+            #region 20190610演示版增加水印效果
+
+            try
+            {
+                Aspose.Cells.Workbook workbook = new Aspose.Cells.Workbook(sourceExcelFile);
+                for (int i = 0; i < workbook.Worksheets.Count; i++)
+                {
+                    int firstRowIndex = workbook.Worksheets[i].FirstVisibleRow;
+                    int rowMaxIndex = workbook.Worksheets[i].Cells.Rows.Count;
+
+                    //int firstColumsIndex = workbook.Worksheets[i].FirstVisibleColumn;
+                    //int columnMaxIndex = workbook.Worksheets[i].Cells.Columns.Count;
+
+                    int row = firstRowIndex+ 1;
+                    while (row < rowMaxIndex+ firstRowIndex)
+                    {
+
+                        AddWaterMarkInSheet(workbook.Worksheets[i], MsoPresetTextEffect.TextEffect2,
+    "演示版", "", 50, false, true
+    , row, 0, 1, 0, 100, 500);
+
+                        row += 30;
+                    }
+
+                }
+
+                workbook.Save(sourceExcelFile);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            #endregion
+        }
+
+
+        void AddWaterMarkInSheet(Worksheet worksheet, MsoPresetTextEffect effect, string text, 
+            string fontName, int size, bool fontBold, bool fontItalic, int upperLeftRow, 
+            int top, int upperLeftColumn, int left, int height, int width)
+        {
+            Aspose.Cells.Drawing.Shape wordart = worksheet.Shapes.AddTextEffect(
+                effect,text, fontName, size, fontBold, fontItalic, upperLeftRow, top, upperLeftColumn, left, height,width);
+
+            MsoFillFormat wordArtFormat = wordart.FillFormat;
+            wordArtFormat.ForeColor = System.Drawing.Color.Red;
+            wordArtFormat.Transparency = 0.9;
+            MsoLineFormat lineFormat = wordart.LineFormat;
+            lineFormat.IsVisible = false;
+            wordart.SetLockedProperty(ShapeLockType.Selection, true);
+            wordart.SetLockedProperty(ShapeLockType.ShapeType, true);
+            wordart.SetLockedProperty(ShapeLockType.Move, true);
+            wordart.SetLockedProperty(ShapeLockType.Resize, true);
+            wordart.SetLockedProperty(ShapeLockType.Text, true);
         }
     }
 }
